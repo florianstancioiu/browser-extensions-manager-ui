1ad7b840f4a74b7cb9cde333c92cfdb85ee37cb7
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2025-05-10 09:32:05 +0300

    Added deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,6 @@ import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/browser-extensions-manager-ui/",
   plugins: [react(), tailwindcss(), svgr()],
 });
